7a79b7f7d2da7977107e970f93fa1bf673e8b73d
Alexminator99 <alexminator1999@gmail.com>
2021-07-05 22:08:55 -0400

    Change constant json to test only.

--- app/src/main/java/com/everything/deckapp/Constants.kt
+++ app/src/main/java/com/everything/deckapp/Constants.kt
@@ -1,4 +1,213 @@
 package com.everything.deckapp
 
 const val cardsDeck =
-    "[{\"suit\": \"spades\", \"value\": \"6\"}, {\"suit\": \"diamonds\", \"value\": \"7\"}, {\"suit\": \"hearts\", \"value\": \"7\"}, {\"suit\": \"spades\", \"value\": \"3\"}, {\"suit\": \"diamonds\", \"value\": \"J\"}, {\"suit\": \"hearts\", \"value\": \"A\"}, {\"suit\": \"spades\", \"value\": \"K\"}, {\"suit\": \"spades\", \"value\": \"4\"}, {\"suit\": \"diamonds\", \"value\": \"K\"}, {\"suit\": \"clubs\", \"value\": \"10\"}, {\"suit\": \"diamonds\", \"value\": \"9\"}, {\"suit\": \"clubs\", \"value\": \"6\"}, {\"suit\": \"spades\", \"value\": \"J\"}, {\"suit\": \"hearts\", \"value\": \"8\"}, {\"suit\": \"clubs\", \"value\": \"3\"}, {\"suit\": \"hearts\", \"value\": \"10\"}, {\"suit\": \"spades\", \"value\": \"A\"}, {\"suit\": \"diamonds\", \"value\": \"A\"}, {\"suit\": \"diamonds\", \"value\": \"2\"}, {\"suit\": \"hearts\", \"value\": \"K\"}, {\"suit\": \"clubs\", \"value\": \"4\"}, {\"suit\": \"hearts\", \"value\": \"Q\"}, {\"suit\": \"spades\", \"value\": \"Q\"}, {\"suit\": \"clubs\", \"value\": \"K\"}, {\"suit\": \"diamonds\", \"value\": \"3\"}, {\"suit\": \"diamonds\", \"value\": \"5\"}, {\"suit\": \"hearts\", \"value\": \"5\"}, {\"suit\": \"hearts\", \"value\": \"3\"}, {\"suit\": \"spades\", \"value\": \"9\"}, {\"suit\": \"diamonds\", \"value\": \"10\"}, {\"suit\": \"clubs\", \"value\": \"2\"}]"
+    "[\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"A\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"2\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"3\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"4\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"5\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"6\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"7\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"8\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"9\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"10\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"J\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"Q\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"clubs\",\n" +
+            "    \"value\": \"K\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"A\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"2\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"3\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"4\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"5\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"6\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"7\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"8\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"9\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"10\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"J\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"Q\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"diamonds\",\n" +
+            "    \"value\": \"K\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"A\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"2\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"3\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"4\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"5\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"6\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"7\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"8\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"9\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"10\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"J\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"Q\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"hearts\",\n" +
+            "    \"value\": \"K\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"A\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"2\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"3\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"4\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"5\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"6\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"7\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"8\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"9\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"10\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"J\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"Q\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"suit\": \"spades\",\n" +
+            "    \"value\": \"K\"\n" +
+            "  }\n" +
+            "]"
