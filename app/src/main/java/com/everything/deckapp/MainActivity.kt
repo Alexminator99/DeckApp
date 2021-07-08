4a7b3e4ab74a11216791d3897ad09f9940de4dec
Alexminator99 <alexminator1999@gmail.com>
2021-07-07 22:24:53 -0400

    Refactored UI only to control visibility

--- app/src/main/java/com/everything/deckapp/MainActivity.kt
+++ app/src/main/java/com/everything/deckapp/MainActivity.kt
@@ -4,95 +4,67 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.everything.deckapp.data.models.UrlInfoRequest
+import com.everything.blogexample.utils.gone
+import com.everything.blogexample.utils.visible
 import com.everything.deckapp.databinding.ActivityMainBinding
-import com.everything.deckapp.utils.Algorithm
+import com.everything.deckapp.utils.CurrentStatus
 import com.everything.deckapp.viewModels.JsonViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val urlInfoRequest = UrlInfoRequest()
 
-    private val viewModel: JsonViewModel by lazy {
-        ViewModelProvider(this).get(JsonViewModel::class.java)
-    }
+    private val viewModel: JsonViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+
         initViewModel()
         init()
     }
 
     private fun initViewModel() {
-        viewModel.jsonData.observe(this) {
-            it.forEach { cardItem ->
-                cardItem.toString()
+        viewModel.shouldUpdateUI.observe(this) {
+            when (it) {
+                is CurrentStatus.Loaded -> {
+                    binding.relativeLayoutLoading.gone()
+                    binding.answerLayout.visible()
+                }
+                is CurrentStatus.Loading -> {
+                    binding.answerCardLayout.visible()
+                    binding.relativeLayoutLoading.visible()
+
+                    binding.answerLayout.gone()
+                    binding.progressIndicator.show()
+                }
+                is CurrentStatus.Error -> {
+                    Log.e("ERROR", it.exception)
+                    binding.answerCardLayout.gone()
+                    Toast.makeText(this, it.exception, Toast.LENGTH_SHORT).show()
+
+                }
             }
-
-            val answer = Algorithm.getAllDecksWithCompleteSize(it)
-
-            binding.relativeLayoutLoading.visibility = View.GONE
-
-            binding.answerLayout.visibility = View.VISIBLE
-
-            binding.textViewAnswerAlgorithm.text = answer.toString()
-            binding.textViewAnswerDecodedText.text = urlInfoRequest.answer
-
-            if (urlInfoRequest.answer != null && urlInfoRequest.answer == answer.toString()) {
-
-                binding.textViewStatusAnswer.text = getString(R.string.correct_answer)
-            } else {
-                binding.textViewStatusAnswer.text = getString(R.string.incorrect_answer)
-            }
-        }
-        viewModel.errorMessage.observe(this) {
-            Log.e("ERROR", it)
-            binding.answerCardLayout.visibility = View.GONE
-            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun init() {
         binding.materialButtonBeginTest.setOnClickListener {
-            urlInfoRequest.clear()
-
-            binding.answerCardLayout.visibility = View.VISIBLE
-            binding.relativeLayoutLoading.visibility = View.VISIBLE
-
-            binding.answerLayout.visibility = View.GONE
-            binding.progressIndicator.show()
-
             if (binding.textInputEditTextUrl.text?.isNotEmpty() == true) {
-
                 val text = binding.textInputEditTextUrl.text.toString()
-                val urlInfoRequest = Algorithm.getUrlInfoRequest(text)
-
-                if (urlInfoRequest != null) {
-                    Toast.makeText(
-                        this,
-                        "Url is correct, loading answer...",
-                        Toast.LENGTH_LONG
-                    )
-                        .show()
-                    viewModel.getDeckRequest(urlInfoRequest)
-                } else {
-                    Toast.makeText(this, "Please paste a valid URL", Toast.LENGTH_SHORT).show()
-                    binding.answerCardLayout.visibility = View.GONE
-                }
-
+                viewModel.getDeckRequest(text)
             } else {
                 Toast.makeText(this, "Please paste a valid URL", Toast.LENGTH_SHORT).show()
                 binding.answerCardLayout.visibility = View.GONE
             }
+
         }
     }
-
-
 }
 
