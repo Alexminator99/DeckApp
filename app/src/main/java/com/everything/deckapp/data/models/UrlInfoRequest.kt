5f89f26adf64d35275ee5727fda2166e98ddf470
Alexminator99 <alexminator1999@gmail.com>
2021-07-11 02:43:51 -0400

    Added toString() method.

--- app/src/main/java/com/everything/deckapp/data/models/UrlInfoRequest.kt
+++ app/src/main/java/com/everything/deckapp/data/models/UrlInfoRequest.kt
@@ -7,7 +7,13 @@ class UrlInfoRequest {
         secretKey = ""
     }
 
+    override fun toString(): String {
+        return "UrlInfoRequest(answer=$answer, url=$url, secretKey='$secretKey')"
+    }
+
     var answer: String? = null // Answer given by url text
     var url: String? = null // URL extracted form text
     var secretKey: String = "" // secret key extracted form text
+
+
 }
