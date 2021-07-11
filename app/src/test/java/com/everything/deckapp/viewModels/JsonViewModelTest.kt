5fbd01b5568df24a4657a1b2422d91a6b26bc9b7
Alexminator99 <alexminator1999@gmail.com>
2021-07-11 02:02:01 -0400

    Added retrofit call. Still in work. Coroutines are not tested like that. That function is only for skeleton

--- app/src/test/java/com/everything/deckapp/viewModels/JsonViewModelTest.kt
+++ app/src/test/java/com/everything/deckapp/viewModels/JsonViewModelTest.kt
@@ -6,6 +6,8 @@ import org.junit.Rule
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.everything.deckapp.data.models.UrlInfoRequest
+import com.everything.deckapp.getOrAwaitValue
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertEquals
@@ -26,7 +28,7 @@ class JsonViewModelTest {
     }
 
     @Test
-    fun getUrlInfoRequest() {
+    fun getUrlInfoRequest_returnsNotNullOneString() {
         /** When requesting [UrlInfoRequest] given an URL */
         val URL = "Ans1OneFullDeckNoRepeated\n" +
                 "https://api.jsonbin.io/b/60e3412155b7245a20d65cea \n"
@@ -38,4 +40,17 @@ class JsonViewModelTest {
         assertEquals("60e3412155b7245a20d65cea", urlInfoRequest?.url)
     }
 
+    @Test
+    fun getUrlInfoRequest_returnsNotNull5StringString() {
+        /** When requesting [UrlInfoRequest] given an URL */
+        val URL = "Ans5 (private)(\"secret-key: \$2b\$10\$ppaKYa7yQfoGUU1hMQXfwevzkNbovRWLWbV4O0NKiTGUcRgcUJAaG\")\n" +
+                "https://api.jsonbin.io/b/60e342b39328b059d7b7801e"
+        val urlInfoRequest = jsonViewModel.getUrlInfoRequest(URL)
+
+        // Then result is not null, is an instance of UrlInfoRequest and answer should be 5 and url key 60e342b39328b059d7b7801e
+        assertThat(urlInfoRequest, `is`(not(nullValue(UrlInfoRequest::class.java))))
+        assertEquals(5.toString(), urlInfoRequest?.answer)
+        assertEquals("60e342b39328b059d7b7801e", urlInfoRequest?.url)
+        assertEquals("\$2b\$10\$ppaKYa7yQfoGUU1hMQXfwevzkNbovRWLWbV4O0NKiTGUcRgcUJAaG", urlInfoRequest?.secretKey)
+    }
 }
